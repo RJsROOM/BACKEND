0c8cabaa16f690052b9d33bbf3b8c4e5743955bd
Rakshit jha <rakshitjha05@gmail.com>
2026-04-05 19:05:07 +0530

    Day2: programming a server

--- Day1/index.js
+++ Day1/index.js
@@ -3,8 +3,9 @@
 // console.log(catme());
 // packagelock.json file stores that whatever code we have written on which it depends and further the package which we have used if that also is dependant on any other package then it is also mentioned in the packagelock.json and all the code files goes in the node module folder.
 // expresss is also a package which lets us create servers seamlesssly and is very easy to use.
-// const express= require('express');
-// const app=express() //server create ho chuka hai
-// app.listen(3000)  //server start ho chuka hai
+
+const express= require('express');
+const app=express() //server create ho chuka hai
+app.listen(3000)  //server start ho chuka hai
 
 // .gitignore file is used to ignore certain files or folders from being tracked by git version control system. For example, node_modules folder is usually ignored as it contains all the packages which can be installed using package.json file and it can take up a lot of space in the repository.
--- Day2/server.js
+++ Day2/server.js
@@ -1,17 +1,21 @@
 const express= require('express');
 
-const app= express(); //server ka instance create hogya
+const app= express();  //server instance is created
+
+
+//server started and listening on port 3000
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000");  
+})
 
 app.get('/', (req,res)=>{
-    res.send('hello world')
+    res.send("Home page");
 })
 
 app.get('/about', (req,res)=>{
-    res.send('this is about page')
+    res.send("About page");
 })
 
-app.get('/home', (req,res)=>{
-    res.send('this is home page')
+app.get('/services', (req,res)=>{
+    res.send("services we offer");
 })
-
-app.listen(3000) // server start hogyacls
