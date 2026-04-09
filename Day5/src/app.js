1233f4e37e89b1d75107e12af2c8e4cd1f909b42
Rakshit jha <rakshitjha05@gmail.com>
2026-04-09 17:27:26 +0530

    Day5: understanding status codes of success category

--- Day5/src/app.js
+++ Day5/src/app.js
@@ -6,32 +6,55 @@ const app= express();
 
 app.use(express.json())
 
+
+
 const notes=[]
 
 app.get("/", (req,res)=>{
     res.send("welcome to Home page")
 })
 
-app.post("/notes", (res,req)=>{
+app.post("/notes", (req,res)=>{
     notes.push(req.body)
-    res.send("note added")
+    res.status(201).json({
+        message: "note created successfully"
+    })
 })
 
 app.get("/notes", (req,res)=>{
-    res.send(notes)
+    res.status(200).json({
+        notes:notes
+    })
 })
 
 app.delete("/notes/:id", (req,res)=>{
     delete notes[req.params.id];
-    res.send("note deleted successfully")
+    res.status(200).json({
+        message: "note deleted successfully"
+    })
 })
 
 app.patch("/notes/:id", (req,res)=>{
     notes[req.params.id].description= req.body.description;
-    res.send("note updated successfully")
+    res.status(200).json({
+        message: "note updated successfully"
+    })
 })
 
 
 
 
 module.exports=app
+
+// coomon status codes:
+/*   
+-------Category: successful responses-------
+ 200: OK the request was successful and the server responded with the requested data
+
+ 201: a new resource has been created successfully
+
+ 204: the request was successfull but there is no content to return
+
+since evrry restart the system gives new RAM to our server and this causes empty notes array for solving this we use the databases for storing the data.
+
+*/
