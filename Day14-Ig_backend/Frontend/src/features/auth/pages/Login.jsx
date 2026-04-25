762b70fe2f55f98e94a59494b538eff104ea3720
Rakshit jha <rakshitjha05@gmail.com>
2026-04-25 21:42:08 +0530

    Day19: login API intergation and UI done

--- Day14-Ig_backend/Frontend/src/features/auth/pages/Login.jsx
+++ Day14-Ig_backend/Frontend/src/features/auth/pages/Login.jsx
@@ -1,16 +1,52 @@
+import { useState } from 'react';
 import '../style/form.scss'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
-const login = () => {
+const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        axios.post("http://localhost:3000/api/auth/login",{
+            username,
+            password
+        },{
+            withCredentials:true
+        })
+        .then(res=>{
+            console.log(res.data)
+        })
+
+    }
+
+
+
   return (
     <main>
         <div className="form-container">
             <h1>
                 Login
             </h1>
-            <form>
-                <input type="text" placeholder="Enter Username" name="username" />
-                <input type="password" placeholder="Enter Password" name="password" />
+            <form onSubmit={handleSubmit}>
+                <input 
+                    onInput={(e)=>setUsername(e.target.value)}
+                    type="text" 
+                    placeholder="Enter Username" 
+                    name="username" 
+                />
+
+                <input 
+                    onInput={(e)=>setPassword(e.target.value)}
+                    type="password" 
+                    placeholder="Enter Password" 
+                    name="password" 
+                />
+
                 <button type="submit">
                     Login
                 </button>
@@ -22,4 +58,4 @@ const login = () => {
   )
 }
 
-export default login
+export default Login
