94030d640a6b8b5df1bbe94a9e5007a271ae2058
Rakshit jha <rakshitjha05@gmail.com>
2026-09-17 10:30:09 +0530

    ..

--- Day25-Moodify/Backend/src/routes/auth.routes.js
+++ Day25-Moodify/Backend/src/routes/auth.routes.js
@@ -13,7 +13,7 @@ router.post('/login', authController.loginUser)
 
 router.get("/get-me", authMiddleware.authUser, authController.getMe)
 
-router.get("/logout", authMiddleware.authUser, authController.logoutUser)
+router.get("/logout", authController.logoutUser)
 
 
 module.exports= router
